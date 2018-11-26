95942b5bb4a32a4294e870497c99848875646332
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-11-26 18:00:04 +0700

    nambahin entitas mahasiswa

---
+++ app/src/main/java/isfaaghyth/app/portalti16/entity/Mahasiswa.java
@@ -0,0 +1,18 @@
+package isfaaghyth.app.portalti16.entity;
+
+/**
+ * Created by isfaaghyth on 26/11/18.
+ * github: @isfaaghyth
+ */
+public class Mahasiswa {
+    private String name;
+    private String nim;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+}
