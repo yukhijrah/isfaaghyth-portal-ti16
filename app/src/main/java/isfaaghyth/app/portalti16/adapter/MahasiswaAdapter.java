5c2cf0b95f70432c5965de00a05095b59e9c56a3
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-03 17:26:46 +0700

    add adapter, holder, and recyclerview

--- app/src/main/java/isfaaghyth/app/portalti16/adapter/MahasiswaAdapter.java
+++ app/src/main/java/isfaaghyth/app/portalti16/adapter/MahasiswaAdapter.java
@@ -1,10 +1,13 @@
 package isfaaghyth.app.portalti16.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
 
+import isfaaghyth.app.portalti16.R;
 import isfaaghyth.app.portalti16.entity.Mahasiswa;
 import isfaaghyth.app.portalti16.holder.MahasiswaHolder;
 
@@ -22,17 +25,20 @@ public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaHolder> {
 
     @Override
     public MahasiswaHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mahasiswa, parent, false);
+        MahasiswaHolder holder = new MahasiswaHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(MahasiswaHolder holder, int position) {
-
+        holder.txtNama.setText(mahasiswas.get(position).getName());
+        holder.txtNim.setText(mahasiswas.get(position).getNim());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mahasiswas.size();
     }
 
 }
--- app/src/main/java/isfaaghyth/app/portalti16/holder/MahasiswaHolder.java
+++ app/src/main/java/isfaaghyth/app/portalti16/holder/MahasiswaHolder.java
@@ -2,6 +2,9 @@ package isfaaghyth.app.portalti16.holder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import isfaaghyth.app.portalti16.R;
 
 /**
  * Created by isfaaghyth on 26/11/18.
@@ -9,8 +12,13 @@ import android.view.View;
  */
 public class MahasiswaHolder extends RecyclerView.ViewHolder {
 
+    public TextView txtNama;
+    public TextView txtNim;
+
     public MahasiswaHolder(View itemView) {
         super(itemView);
+        txtNama = (TextView) itemView.findViewById(R.id.txt_nama);
+        txtNim = (TextView) itemView.findViewById(R.id.txt_nim);
     }
 
 }
