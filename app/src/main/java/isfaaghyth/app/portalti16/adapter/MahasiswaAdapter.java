c229c72e34a179e521a4a00b9a2402e43a293db8
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-17 16:57:57 +0700

    add button action on mahasiswaAdapter

--- app/src/main/java/isfaaghyth/app/portalti16/adapter/MahasiswaAdapter.java
+++ app/src/main/java/isfaaghyth/app/portalti16/adapter/MahasiswaAdapter.java
@@ -1,5 +1,7 @@
 package isfaaghyth.app.portalti16.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.ViewGroup;
 
 import java.util.List;
 
+import isfaaghyth.app.portalti16.DetailMahasiswaActivity;
 import isfaaghyth.app.portalti16.R;
 import isfaaghyth.app.portalti16.entity.Mahasiswa;
 import isfaaghyth.app.portalti16.holder.MahasiswaHolder;
@@ -32,6 +35,16 @@ public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaHolder> {
     public MahasiswaHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mahasiswa, parent, false);
         MahasiswaHolder holder = new MahasiswaHolder(view);
+
+        final Context context = holder.itemView.getContext();
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent = new Intent(context, DetailMahasiswaActivity.class);
+                context.startActivity(detailIntent);
+            }
+        });
+
         return holder;
     }
 
